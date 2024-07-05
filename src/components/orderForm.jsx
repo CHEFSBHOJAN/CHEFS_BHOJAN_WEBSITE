90e7d0b219a39d7a04f7ff2d2f5395ebab31ece0
nagesh-merva <officialnagesh.merva@gmail.com>
2024-07-05 21:21:01 +0530

    modified

--- src/components/orderForm.jsx
+++ src/components/orderForm.jsx
@@ -67,6 +67,7 @@ function OrderForm() {
                 headers: {
                     'Content-Type': 'application/json'
                 },
+                credentials: 'include',
                 body: JSON.stringify(newOrder)
             });
 
