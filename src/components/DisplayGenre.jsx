d170af9fcb800c01904ecd185a4406875938d5bf
CHEFSBHOJAN <bhojanchefs@gmail.com>
2024-07-15 15:17:09 +0530

    Update DisplayGenre.jsx

--- src/components/DisplayGenre.jsx
+++ src/components/DisplayGenre.jsx
@@ -59,7 +59,7 @@ function DisplayGenre() {
                 <h1 className="mb-4 px-5 py-3 bg-amber-400 rounded-full font-poppins text-2xl text-white font-semibold ">Items of {genre.toUpperCase()}</h1>
                 <div className="flex flex-wrap justify-center">
                     {filteredItems.map((dish, index) => (
-                        <div key={index} className="dish-item px-2 py-4 grid grid-cols-2 gap-2 border-b-2 border-dashed">
+                        <div key={index} className="dish-item px-2 py-4 grid grid-cols-2 gap-2 items-center border-b-2 border-dashed">
                             <div className="dish-info" >
                                 <div className="dish-type-icons">
                                     {dish.type === 'veg' ? (
@@ -72,7 +72,7 @@ function DisplayGenre() {
                                 <p className='py-1 px-1 text-lg text-black font-semibold'>₹{dish.price}</p>
                                 <p className='py-1 px-1 text-[10px] text-slate-500 font-thin'>{dish.description}</p>
                             </div>
-                            <div className='relative h-48 w-48 flex justify-center'>
+                            <div className='relative h-48 w-full flex justify-center'>
                                 <img src={dish.img} className='h-40 w-40 rounded-3xl drop-shadow-xl shadow-slate-600' loading="lazy" alt="" />
                                 <button onClick={() => addToCart(dish)} className='absolute bottom-4 left-1/2 transform -translate-x-1/2 px-10 py-2 text-xl bg-slate-100/90 text-amber-500 font-poppins font-bold border-2 border-amber-500 rounded-xl'>ADD</button>
                             </div>
